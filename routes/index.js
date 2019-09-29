b3c7e400e69616a9a642a9efce8dec11e5c2808e
yuhua_qin@xinyan.com <yuhua_qin@xinyan.com>
2019-09-29 17:25:56 +0800

    初始化

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const mongoose = require('mongoose');
+
+// 这个地方如果没有对应数据库没有关系，在执行响应的mongoose操作数据库的命令的时候会自动创建数据库的
 // mongodb://user:password@127.0.0.1:27017/dbname
 // dbname-必选项 user/password 是可选的
-const url = 'mongodb://127.0.0.1:27017/admin'; // 本地数据库地址
+const url = 'mongodb://127.0.0.1:27017/blog'; // 本地数据库地址
 
 /*
 * connect(url, option)  参数，这些参数 会传入底层MongoDB 驱动
@@ -26,6 +28,5 @@ db.on('errror', () => {
 db.on('disconnected', () => {
   console.log('MongoDB connected to disconnected');
 });
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/*
+* 新增
+* */
+router.post("/insert", (req, res, next) => {
+  const { userName } = req.body;
+  User.findOne({ userName }, (err, doc) => {
+    if(err) {
+      res.json({
+        success: false,
+        message: err.message,
+      });
+    } else {
+      if (doc) {
+        res.json({
+          success: false,
+          message: `${doc.userName}已存在，不能重复添加`,
+        });
+      } else {
+        req.body.createAt = new Date().getTime();
+        const data = new User(req.body);
+        // save 文档新增
+        data.save((err1, doc1) => {
+          if (err1) {
+            res.json({
+              success: false,
+              message: err1.message
+            });
+          } else {
+            res.json({
+              success: true,
+              message: '添加成功'
+            });
+          }
+        });
+      }
+    }
+  });
 });
+
 
 module.exports = router;
